e315e91c46381d3cd1e6377d4f1332e236cecb48
s3remotica <s3remotica@gmail.com>
2024-11-13 11:30:56 +0530

    update

---
+++ precache-manifest.61f3e53f143b001aad80d594f68f1b6f.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3335d6550bbcd9204b047623854083",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c009f2ce3080757b204",
+    "url": "/static/css/2.eaa86fc5.chunk.css"
+  },
+  {
+    "revision": "bb7f5e24f1d164f54921",
+    "url": "/static/css/main.9fa73777.chunk.css"
+  },
+  {
+    "revision": "6c009f2ce3080757b204",
+    "url": "/static/js/2.7ccc486e.chunk.js"
+  },
+  {
+    "revision": "9a87d441d6f1d11956d517a23294f3df",
+    "url": "/static/js/2.7ccc486e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6bbaf3e3b089acd27e",
+    "url": "/static/js/3.62ccc47a.chunk.js"
+  },
+  {
+    "revision": "bb7f5e24f1d164f54921",
+    "url": "/static/js/main.950282f0.chunk.js"
+  },
+  {
+    "revision": "299d03f43408f8575398",
+    "url": "/static/js/runtime-main.98bfa66e.js"
+  },
+  {
+    "revision": "f0b781e3fd22ed6ec08f3be4e3fdfd78",
+    "url": "/static/media/Designing-A-Website-2--Streamline-Brooklyn.f0b781e3.svg"
+  },
+  {
+    "revision": "bb2ea72378f08c5bf370b0eb79458534",
+    "url": "/static/media/Ellipse.bb2ea723.svg"
+  },
+  {
+    "revision": "03ae2efc18bbe2d8e17d07c534df4b8d",
+    "url": "/static/media/QIcon.03ae2efc.svg"
+  },
+  {
+    "revision": "095c9b2df74acfef48c1b706aea53d7b",
+    "url": "/static/media/arrow-left.095c9b2d.svg"
+  },
+  {
+    "revision": "5d41c91a203494b6ce3101930f8897b5",
+    "url": "/static/media/arrow-right.5d41c91a.svg"
+  },
+  {
+    "revision": "e993ed09751e45cddac368d294f82f25",
+    "url": "/static/media/briefcase.e993ed09.svg"
+  },
+  {
+    "revision": "4c38875a7f34c52149b3aa7ffdd39e0a",
+    "url": "/static/media/chart.4c38875a.svg"
+  },
+  {
+    "revision": "4b8f7add4eb73d17112a621a161955f8",
+    "url": "/static/media/code-circle.4b8f7add.svg"
+  },
+  {
+    "revision": "2ea66ddd2e51ccdb1406c3e6d1587d3a",
+    "url": "/static/media/courses.2ea66ddd.svg"
+  },
+  {
+    "revision": "1a7c361e54f7b7fff6f828b3b164c1b4",
+    "url": "/static/media/laptolease-innovation.1a7c361e.svg"
+  },
+  {
+    "revision": "d7cb0e84721cc11706f5327b93a9652f",
+    "url": "/static/media/logo.d7cb0e84.svg"
+  },
+  {
+    "revision": "ac7f6ddcb8e98049742fa658c5c4ce12",
+    "url": "/static/media/messages.ac7f6ddc.svg"
+  },
+  {
+    "revision": "e7f3dca16c3475326336e376b06665df",
+    "url": "/static/media/profile.e7f3dca1.svg"
+  },
+  {
+    "revision": "b314840f807378c7ec21538cd0664d63",
+    "url": "/static/media/qbatzLetter.b314840f.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c5fb1aa4c5dd7980666ba1a0216f28f6",
+    "url": "/static/media/smartstay-innovation.c5fb1aa4.svg"
+  }
+]);
